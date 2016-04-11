455854c759e9c6755da6f6633336a349ad22a41d
sharanya24 <sharanya.kandikatla@imaginea.com>
2016-04-11 18:27:19 +0530

    Update ToH.scala

--- src/com/pramati/main/ToH.scala
+++ src/com/pramati/main/ToH.scala
@@ -2,17 +2,16 @@ package com.pramati.main
 
 class TowerOfhanoi {
 
-  def moveDisks(nDisk: Int, src: String, temp: String, des: String): String = {
+  def moveDisks(nDisk: Int, src: String, temp: String, des: String): Unit = {
 
     if (nDisk == 1) {
-
-      return "Move disk from " + src + " to " + des + "\n";
+      println("Move disk from " + src + " to " + des);
     } else {
 
       var res0: String = moveDisks(nDisk - 1, src, des, temp); //pull top n-1 disk from src to destin
-      var str: String = ("Move Disk from " + src + " to " + des + "\n").toString;
+      println("Move disk from " + src + " to " + des);
       var res1: String = moveDisks(nDisk - 1, temp, src, des); //pull n-1 disk from interim to destin
-      return (res0 + str + res1)toString;
+      
 
     }
 
